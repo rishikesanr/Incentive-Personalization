39a05e5288f728fbc1ebcd0693ea579f973cb96b
Rishikesan "Rishi" Ravichandran <61989009+rishikesanr@users.noreply.github.com>
2024-09-16 15:21:13 -0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -127,7 +127,6 @@ elif menu == "Workspaces":
             
             # If "%" is selected for customer incentive
             if customer_incentive == "%":
-                st.write("### Optimization Constraints:")
                 
                 # Percentage of users who receive coupons
                 percent_coupons = st.slider("% of users who receive coupons", 0, 100, 50)
